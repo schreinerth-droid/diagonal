c506b1bb7361f7876a5102f632f07dff32acde5a
schreinerth-droid <schreinerth@gmail.com>
2026-03-25 17:02:03 +0100

    Add files via upload

---
+++ upcoming.js
@@ -0,0 +1,10 @@
+var UPCOMING=[
+  {j:22,date:'2026-03-22',home:false,rival:'Escola F Angel Pedraza A'},
+  {j:23,date:'2026-03-29',home:true, rival:'Les Corts de Barcelona Club Esp. A'},
+  {j:24,date:'2026-04-12',home:false,rival:'Pª Barc. Anguera C'},
+  {j:25,date:'2026-04-19',home:true, rival:'Escola de Futbol Premier Barcelona A'},
+  {j:26,date:'2026-04-26',home:false,rival:'Sants, UE. B'},
+  {j:27,date:'2026-05-03',home:true, rival:'Escola Collblanc-Torrassa, CE A'},
+  {j:28,date:'2026-05-10',home:false,rival:"L'Hospitalet, Centre Esports C"},
+  {j:29,date:'2026-05-17',home:true, rival:'Sarrià, CP. B'}
+];
