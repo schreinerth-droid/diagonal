c8469dff74cf89a69dc0f25524d261716fb6ec40
schreinerth-droid <schreinerth@gmail.com>
2026-03-30 20:34:23 +0200

    Update standings.js

--- data/standings.js
+++ data/standings.js
@@ -1,1 +1,18 @@
-
+var STANDINGS = [
+  { pos:1,  team:'ESCOLA DE FUTBOL PREMIER BARCELONA A', pts:58, j:23, g:18, e:4,  p:1,  gf:75, gc:24 },
+  { pos:2,  team:'Pª BARC. ANGUERA C',                    pts:52, j:23, g:16, e:4,  p:3,  gf:66, gc:20 },
+  { pos:3,  team:"L'HOSPITALET, CENTRE ESPORTS C",       pts:50, j:23, g:14, e:8,  p:1,  gf:93, gc:21 },
+  { pos:4,  team:'APA POBLE SEC, CE. A',                 pts:45, j:23, g:14, e:3,  p:6,  gf:59, gc:28 },
+  { pos:5,  team:'ESCOLA COLLBLANC-TORRASSA,C.E A',      pts:40, j:23, g:12, e:4,  p:7,  gf:66, gc:42 },
+  { pos:6,  team:'EUROPA, C.E. C',                       pts:38, j:22, g:11, e:5,  p:6,  gf:47, gc:32 },
+  { pos:7,  team:'PENYA BARC. COLLBLANC SANTS,AE A',     pts:36, j:23, g:10, e:6,  p:7,  gf:44, gc:33 },
+  { pos:8,  team:'ESCOLA F ANGEL PEDRAZA A',             pts:36, j:23, g:11, e:3,  p:9,  gf:50, gc:49 },
+  { pos:9,  team:'SARRIÀ, C.P. B',                       pts:33, j:23, g:9,  e:6,  p:8,  gf:51, gc:55 },
+  { pos:10, team:'DIAGONAL CLUB ESP. A',                 pts:32, j:22, g:10, e:2,  p:10, gf:27, gc:29 },
+  { pos:11, team:'CAN VIDALET, C.F. A',                  pts:27, j:23, g:8,  e:3,  p:12, gf:23, gc:38 },
+  { pos:12, team:"FUNDACIÓ ACADEMIA F. L'HOSPITALET B",  pts:23, j:23, g:6,  e:5,  p:12, gf:19, gc:34 },
+  { pos:13, team:'SANT IGNASI C.E. A',                   pts:21, j:22, g:6,  e:3,  p:13, gf:32, gc:54 },
+  { pos:14, team:'HOSPITALENSE, AT.C. A',                pts:13, j:23, g:4,  e:1,  p:18, gf:30, gc:70 },
+  { pos:15, team:'LES CORTS DE BARCELONA CLUB ESP. A',   pts:12, j:22, g:3,  e:3,  p:16, gf:29, gc:90 },
+  { pos:16, team:'SANTS, U.E. B',                        pts:0,  j:23, g:0,  e:0,  p:23, gf:10, gc:102 }
+];
