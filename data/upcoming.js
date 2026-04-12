d3127682ef40746e0e28fd84bf677a5e65a403fe
schreinerth-droid <schreinerth@gmail.com>
2026-04-12 19:08:31 +0200

    Update upcoming.js

--- data/upcoming.js
+++ data/upcoming.js
@@ -1,7 +1,5 @@
 var UPCOMING=[
-  {j:22,date:'2026-03-22',home:false,rival:'Escola F Angel Pedraza A'},
-  {j:23,date:'2026-03-29',home:true, rival:'Les Corts de Barcelona Club Esp. A'},
-  {j:24,date:'2026-04-12',home:false,rival:'Pª Barc. Anguera C'},
+  {j:23,date:'2026-04-21',home:true, rival:'Les Corts de Barcelona Club Esp. A'},
   {j:25,date:'2026-04-19',home:true, rival:'Escola de Futbol Premier Barcelona A'},
   {j:26,date:'2026-04-26',home:false,rival:'Sants, UE. B'},
   {j:27,date:'2026-05-03',home:true, rival:'Escola Collblanc-Torrassa, CE A'},
