2e45801769df07c432521ef2157f2449efbcf797
schreinerth-droid <schreinerth@gmail.com>
2026-04-12 19:23:04 +0200

    Update matches.js

--- data/matches.js
+++ data/matches.js
@@ -142,20 +142,13 @@ var MATCHES = [
    titulars:[{id:'steven',d:1},{id:'jordi',d:5},{id:'patricio',d:6},{id:'fj',d:7},{id:'pau',d:8},{id:'gabriel',d:15},{id:'yannick',d:18},{id:'redon',d:22},{id:'mihail',d:29},{id:'santi_e',d:76},{id:'santiago',d:77}],
    suplents:[{id:'marin',d:2},{id:'mikel',d:4},{id:'otto',d:10},{id:'jorge',d:11},{id:'lukas',d:12},{id:'bruno',d:17},{id:'tobias',d:21}],
    goals:[{min:13,type:'gf',playerId:'yannick'},{min:37,type:'gf',playerId:'gabriel'},{min:62,type:'gf',playerId:'jorge'}],
-   yellows:[{playerId:'jordi',d:5}],reds:[]}
-,
+   yellows:[{playerId:'jordi',d:5}],reds:[]},
 
   {j:22,date:'2026-03-22',loc:'F',home:'ESCOLA F ANGEL PEDRAZA A',away:'DIAGONAL CLUB ESP. A',gf:1,gc:3,res:'P',
    ref:'Barón Ajenjo, David',refDel:'Barcelona3',
    titulars:[{id:'steven',d:1},{id:'mikel',d:4},{id:'patricio',d:6},{id:'fj',d:7},{id:'otto',d:10},{id:'lukas',d:12},{id:'gabriel',d:15},{id:'bruno',d:17},{id:'yannick',d:18},{id:'tobias',d:21},{id:'santiago',d:77}],
    suplents:[{id:'jordi',d:5},{id:'jorge',d:11},{id:'luca',d:19},{id:'redon',d:22},{id:'mihail',d:29}],
    goals:[{min:14,type:'gf',playerId:'yannick'},{min:17,type:'gc',playerId:null},{min:52,type:'gc',playerId:null},{min:54,type:'gc',playerId:null}],
-   yellows:[{playerId:'patricio',d:6}],reds:[{playerId:'steven',d:1}]}
+   yellows:[{playerId:'patricio',d:6}],reds:[{playerId:'steven',d:1}]},
 
   {j:24,date:'2026-04-11',loc:'F',home:'Pª Barc. Anguera C',away:'Diagonal',gf:2,gc:4,res:'P',
-   ref:'Redondo Carrera, Benjamin',refDel:'Barcelona1',
-   titulars:[],
-   suplents:[],
-   goals:[{min:4,type:'gf',playerId:'yannick'},{min:23,type:'gf',playerId:'santiago'},{min:22,type:'gc',playerId:null},{min:28,type:'gc',playerId:null},{min:36,type:'gc',playerId:null},{min:60,type:'gc',playerId:null}],
-   yellows:[],reds:[]}
-];
