bbb43cd0809b6a5162b62e97a8b000e92ddd0355
schreinerth-droid <schreinerth@gmail.com>
2026-04-12 20:17:46 +0200

    Update matches.js

--- data/matches.js
+++ data/matches.js
@@ -148,7 +148,7 @@ var MATCHES = [
    titulars:[{id:'steven',d:1},{id:'mikel',d:4},{id:'patricio',d:6},{id:'fj',d:7},{id:'otto',d:10},{id:'lukas',d:12},{id:'gabriel',d:15},{id:'bruno',d:17},{id:'yannick',d:18},{id:'tobias',d:21},{id:'santiago',d:77}],
    suplents:[{id:'jordi',d:5},{id:'jorge',d:11},{id:'luca',d:19},{id:'redon',d:22},{id:'mihail',d:29}],
    goals:[{min:14,type:'gf',playerId:'yannick'},{min:17,type:'gc',playerId:null},{min:52,type:'gc',playerId:null},{min:54,type:'gc',playerId:null}],
-   yellows:[{playerId:'patricio',d:6}],reds:[{playerId:'steven',d:1}]}
+   yellows:[{playerId:'patricio',d:6}],reds:[{playerId:'steven',d:1}]},
 
   {j:24,date:'2026-04-11',loc:'F',home:'Pª Barc. Anguera C',away:'Diagonal',gf:2,gc:4,res:'P',
    ref:'Redondo Carrera, Benjamin',refDel:'Barcelona1',
@@ -156,3 +156,4 @@ var MATCHES = [
    suplents:[{id:'marin',d:2},{id:'fj',d:7},{id:'otto',d:10},{id:'jorge',d:11},{id:'santi_e',d:76}],
    goals:[{min:4,type:'gf',playerId:'yannick'},{min:23,type:'gf',playerId:'santiago'},{min:22,type:'gc',playerId:null},{min:28,type:'gc',playerId:null},{min:36,type:'gc',playerId:null},{min:60,type:'gc',playerId:null}],
    yellows:[],reds:[]}
+];
