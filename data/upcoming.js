e05b0c5e5d2b02a3bde24cc351982b07ce94bb23
schreinerth-droid <schreinerth@gmail.com>
2026-04-22 09:52:17 +0200

    Update upcoming.js

--- data/upcoming.js
+++ data/upcoming.js
@@ -1,6 +1,7 @@
-var UPCOMING=[
-  {j:26,date:'2026-04-26',home:false,rival:'Sants, UE. B'},
-  {j:27,date:'2026-05-03',home:true, rival:'Escola Collblanc-Torrassa, CE A'},
-  {j:28,date:'2026-05-10',home:false,rival:"L'Hospitalet, Centre Esports C"},
-  {j:29,date:'2026-05-17',home:true, rival:'Sarrià, CP. B'}
+var UPCOMING = [
+  {j:26,date:'2026-04-26',home:false,rival:'Sants UE B'},
+  {j:27,date:'2026-05-03',home:true,rival:'Escola Collblanc-Torrassa CE A'},
+  {j:28,date:'2026-05-10',home:false,rival:"L'Hospitalet CE C"},
+  {j:29,date:'2026-05-17',home:true,rival:'Sarrià CP B'},
+  {j:30,date:'2026-05-24',home:false,rival:'Can Vidalet CF A'}
 ];
