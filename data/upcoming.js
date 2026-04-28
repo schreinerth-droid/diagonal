dafad9b0a3284e93ae6dd2fb97a5b3d0597a2085
schreinerth-droid <schreinerth@gmail.com>
2026-04-28 11:41:02 +0200

    Update upcoming.js

--- data/upcoming.js
+++ data/upcoming.js
@@ -1,5 +1,4 @@
 var UPCOMING = [
-  {j:26,date:'2026-04-26',home:false,rival:'Sants UE B'},
   {j:27,date:'2026-05-03',home:true,rival:'Escola Collblanc-Torrassa CE A'},
   {j:28,date:'2026-05-10',home:false,rival:"L'Hospitalet CE C"},
   {j:29,date:'2026-05-17',home:true,rival:'Sarrià CP B'},
